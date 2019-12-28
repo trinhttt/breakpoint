d57a7da62a6e0ff737e4f74fefc79893894643db
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2019-12-28 15:05:59 +0700

    Init project & add firebase

---
+++ breakpoint/breakpoint/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  breakpoint
+//
+//  Created by Trinh Thai on 12/28/19.
+//  Copyright © 2019 Trinh Thai. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ breakpoint/breakpoint/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  breakpoint
+//
+//  Created by Trinh Thai on 12/28/19.
+//  Copyright © 2019 Trinh Thai. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
